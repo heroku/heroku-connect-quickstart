db29153044372261c125bd9d4ec3fe987a3906fd
Marty Alchin <marty@martyalchin.com>
2015-02-18 11:36:29 -0800

    Create web.py

---
+++ web.py
@@ -0,0 +1,21 @@
+import os
+import re
+
+import flask
+
+app = flask.Flask(__name__)
+host_re = re.compile(r'^([^.]+)\.herokuapp\.com$')
+CREATE_URL = 'https://connect.heroku.com/dashboard-next/create-connection'
+
+
+@app.route('/create')
+def create_redirect():
+    match = host_re.match(flask.request.host)
+    if match:
+        flask.redirect(CREATE_URL + '?create=%s' % match.group(1))
+    return flask.redirect(CREATE_URL)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
